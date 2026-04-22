6420dce36c403acd46fdf81f5cb0cf0327175585
jwalton-csumb <jwalton@csumb.edu>
2026-04-22 15:18:40 -0700

    Basic login screen class, I need to continue updating it.

---
+++ src/main/java/LoginScreenController.java
@@ -0,0 +1,27 @@
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+public class LoginScreenController {
+
+    //private final DatabaseManager db = DatabaseManager.getInstance();
+    //TODO commented this cause I was writing it before it's implemented
+
+    public Scene buildScene(){
+        Label title = new Label("Login Page");
+        TextField usernameField = new TextField();
+        PasswordField passwordField = new PasswordField();
+        Button loginButton = new Button("Login!");
+        TextArea newUser = new TextArea("New user? Sign up below!");
+        Button signUp = new Button("Sign up!");
+
+
+        VBox root = new VBox(10, title, usernameField, passwordField, loginButton, newUser, signUp);
+        root.setPadding(new Insets(16));
+
+        //TODO loginButton.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.{FIX THIS SPOT HERE}));)
+        //TODO signUpButton.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.{FIX THIS SPOT HERE}));)
+    }
+
+}
