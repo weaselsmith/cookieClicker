233b7e36256c54ffb6ebbc832daf81877fdbb4a5
jwalton-csumb <jwalton@csumb.edu>
2026-04-22 15:37:53 -0700

    Added signup screen separate from login screen

--- src/main/java/SignUpScreenController.java
+++ src/main/java/SignUpScreenController.java
@@ -3,23 +3,20 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
-public class LoginScreenController {
+public class SignUpScreenController {
 
     //private final DatabaseManager db = DatabaseManager.getInstance();
     //TODO commented this cause I was writing it before it's implemented
 
     public Scene buildScene(){
-        Label title = new Label("Login Page");
-        TextField usernameField = new TextField();
-        PasswordField passwordField = new PasswordField();
-        Button loginButton = new Button("Login!");
-        TextArea newUser = new TextArea("New user? Sign up below!");
-        Button signUp = new Button("Sign up!");
+        Label title = new Label("Sign Up Page!");
+        TextField newName = new TextField();
+        PasswordField newPassword = new PasswordField();
+        Button signUpButton = new Button("Sign Up!");
 
-        VBox root = new VBox(10, title, usernameField, passwordField, loginButton, newUser, signUp);
+        VBox root = new VBox(10, title, newName, newPassword, signUpButton);
         root.setPadding(new Insets(16));
 
-        //TODO loginButton.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.{FIX THIS SPOT HERE}));)
         //TODO signUpButton.setOnAction(e -> SceneManager.getInstance().navigateTo(SceneType.{FIX THIS SPOT HERE}));)
 
         //refresh();
@@ -28,5 +25,6 @@ public class LoginScreenController {
         return new Scene(root, 640, 480);
 
     }
+
 
 }
