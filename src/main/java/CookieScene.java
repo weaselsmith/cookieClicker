3734cbdb8d0071e45790e085004b8552fda547eb
Daniel <dmtz3471@gmail.com>
2026-04-22 01:02:00 -0700

    Updated CookieScene to shift to Main

--- src/main/java/CookieScene.java
+++ src/main/java/CookieScene.java
@@ -1,5 +1,21 @@
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class CookieScene {
+    public static Scene create(Stage stage) {
+        VBox root = new VBox();
+        Button btn = new Button("New Cookie Scene!");
+
+        btn.setOnAction(e ->
+                SceneManager.getInstance().navigateTo(SceneType.MAIN)
+        );
+
+        root.getChildren().add(btn);
+        return new Scene(new StackPane(btn), 640, 480);
+    }
 
 }
 
