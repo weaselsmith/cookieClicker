38bb4acb967b70617640dbd96e8ca4c1ba798c0f
Daniel <dmtz3471@gmail.com>
2026-04-23 00:13:56 -0700

    Added a counter to CookieScene

--- src/main/java/CookieScene.java
+++ src/main/java/CookieScene.java
@@ -1,20 +1,29 @@
+import javafx.geometry.Pos; //?
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class CookieScene {
+
+    private static int count = 0; // belongs to this scene
+
     public static Scene create(Stage stage) {
-        VBox root = new VBox();
-        Button btn = new Button("New Cookie Scene!");
+        Text counterText = new Text("Score: 0");
+        Button button = new Button("+1");
 
-        btn.setOnAction(e ->
-                SceneManager.getInstance().navigateTo(SceneType.MAIN)
-        );
+        button.setOnAction(e -> {
+            count++;
+            counterText.setText("Score: " + count);
+        });
 
-        root.getChildren().add(btn);
-        return new Scene(new StackPane(btn), 640, 480);
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().addAll(counterText, button);
+
+        return new Scene(root, 640, 480);
     }
 
 }
