3a313c36741191e5ea460d67c728a801de1de270
Noah Panec <nmpanec@gmail.com>
2026-04-26 21:12:39 -0700

    dbmanager testing class scaffolding. no tests written yet.

---
+++ src/test/java/DatabaseManagerTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseManagerTest {
+
+    @Test
+    void addUpgrade() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void addGame() {
+    }
+
+    @Test
+    void addPurchase() {
+    }
+}
