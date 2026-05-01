c1878a26d05e5b02d2c2516442b1c44e098d74be
Daniel <dmtz3471@gmail.com>
2026-05-01 00:30:57 -0700

    Created FileScreen and set up style.css

---
+++ src/main/java/FileScreen.java
@@ -0,0 +1,59 @@
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import model.Game;
+
+import java.awt.*;
+
+import static java.awt.SystemColor.text;
+
+public class FileScreen {
+
+
+
+    public static Scene create(Stage stage){
+
+        Text text1 = new Text("File 1");
+        Text text2 = new Text("File 2");
+        Text text3 = new Text("File 3");
+
+
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.getStylesheets().add(
+                FileScreen.class.getResource("/style.css").toExternalForm()
+        );
+
+        HBox file1 = new HBox();
+        file1.setAlignment(Pos.CENTER);
+        file1.setPrefSize(100,100);
+        file1.getStyleClass().add("hbox-bordered");
+        file1.getChildren().add(text1);
+
+        HBox file2 = new HBox();
+        file2.setAlignment(Pos.CENTER);
+        file2.setPrefSize(100,100);
+        file2.getStyleClass().add("hbox-bordered");
+        file2.getChildren().add(text2);
+
+
+        HBox file3 = new HBox();
+        file3.setAlignment(Pos.CENTER);
+        file3.setPrefSize(100,100);
+        file3.getStyleClass().add("hbox-bordered");
+        file3.getChildren().add(text3);
+
+
+
+        root.getChildren().addAll(file1,file2,file3);
+        return new Scene(root, 640, 480);
+    }
+
+}
--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -13,6 +13,7 @@ import model.Upgrade;
 import java.util.List;
 
 public class SceneFactory {
+
     public static Scene create(SceneType type, Stage stage, DatabaseManager db) {
         return switch (type) {
             case MAIN -> buildMainScene(stage, db);
@@ -34,6 +35,7 @@ public class SceneFactory {
     private static Scene buildDashboardScene(Stage stage, DatabaseManager db) {
         Button btn = new Button("Dashboard");
         Button cookiekBtn = new Button("Navigate to Cookie Scene");
+        Button fileBtn = new Button("Navigate to File Screen");
 
         btn.setOnAction(e ->
                 SceneManager.getInstance().navigateTo(SceneType.ADD_ITEM)
@@ -43,9 +45,13 @@ public class SceneFactory {
                 stage.setScene(CookieScene.create(stage))
         );
 
+        fileBtn.setOnAction(e ->
+                stage.setScene(FileScreen.create(stage))
+        );
+
         VBox vBox = new VBox(10);
         vBox.setAlignment(Pos.CENTER);
-        vBox.getChildren().addAll(btn, cookiekBtn);
+        vBox.getChildren().addAll(btn, fileBtn, cookiekBtn);
 
         return new Scene(vBox, 640, 480);
     }
