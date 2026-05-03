65d3fbf01f7f6460683f3426030f87a23a5ecebe
Noah Panec <nmpanec@gmail.com>
2026-05-02 18:24:05 -0700

    junit tests for purchases

--- src/test/java/GameDriverTest.java
+++ src/test/java/GameDriverTest.java
@@ -1,5 +1,6 @@
 import makers.Grandmas;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.Game;
@@ -21,11 +22,6 @@ class GameDriverTest {
         driver_loaded = new GameDriver(tuple);
     }
 
-    @AfterEach
-    void teardown() {
-
-    }
-
     @Test
     void addCookie() {
         assertEquals(0, driver_new.getCookies());
@@ -39,13 +35,19 @@ class GameDriverTest {
     @Test
     void cookiesPerSecond() {
         assertEquals(50, driver_loaded.calculateCps());
-        driver_loaded.startAutoCookies();
     }
 
     @Test
     void grandmas() {
         Grandmas grandmas = driver_loaded.getGrandmas();
         assertEquals(256, grandmas.getPriceForNext());
+        assertTrue(driver_loaded.buyUnit(MakerType.GRANDMA));
+        assertEquals(744, driver_loaded.getCookies());
+        assertEquals(11, grandmas.getNum());
+        assertTrue(driver_loaded.upgradeUnit(MakerType.GRANDMA, 700));
+        assertEquals(4, grandmas.getLvl());
+        assertEquals(44, driver_loaded.getCookies());
+        assertFalse(driver_loaded.buyUnit(MakerType.GRANDMA));
     }
 
     @Test
