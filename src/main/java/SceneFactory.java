f2569d90db3885b0f34cee35ad2c6bfb51e5e31f
Noah Panec <nmpanec@gmail.com>
2026-05-05 10:31:21 -0700

    switch statement calls class's create methods directly.  Updated to match enum.

--- src/main/java/SceneFactory.java
+++ src/main/java/SceneFactory.java
@@ -5,101 +5,37 @@ import javafx.stage.Stage;
 public class SceneFactory {
     public static Scene create(SceneType type, Stage stage, DatabaseManager db, Context context) {
         return switch (type) {
-            case LOGIN -> buildLoginScene(stage, db, context);
-            case SIGNUP -> buildSignUpScene(stage, db, context);
-            case MENU -> buildMenuScene(stage, db, context);
-            case COOKIE -> buildCookieScene(stage, db, context);
+            case LOGIN -> LoginScreenController.buildScene(db, context);
+            case SIGNUP -> SignUpScreenController.buildScene(db, context);
+            case FILE  -> FileScreen.create(stage, db);
+            case COOKIE -> CookieScene.create(stage, db);
             case STORE -> buildStoreScene(stage, db, context);
             case STATS -> buildStatsScene(stage, db, context);
             case CREDITS -> buildCreditsScene(stage, db, context);
-            case FILE -> null; // this one is a placeholder to avoid error
         };
-    }
-
-    private static Scene buildLoginScene(Stage stage, DatabaseManager db, Context context) {
-        // LoginScreenController either needs buildScene to be static, or it needs a constructor
-        // Static Call:
-        return LoginScreenController.buildScene(db, context);
-        // replaces line below
-        //return new Scene(new VBox(), 640, 480);  // this is a dummy return val
-    }
-
-    private static Scene buildSignUpScene(Stage stage, DatabaseManager db, Context context) {
-        // Static Call:
-        return SignUpScreenController.buildScene(db, context);
-    }
-
-    private static Scene buildMenuScene(Stage stage, DatabaseManager db, Context context) {
-        BorderPane root = new BorderPane();  // doesn't have to stay borderPane
-        // other stuff can go here if needed
-        root.setBottom(NavBar.create(SceneType.MENU));
-        return new Scene(root, 640, 480);
-    }
-
-    private static Scene buildCookieScene(Stage stage, DatabaseManager db, Context context) {
-        return CookieScene.create(stage, db);
     }
 
     private static Scene buildStoreScene(Stage stage, DatabaseManager db, Context context) {
         BorderPane root = new BorderPane();  // doesn't have to stay borderPane
         // other stuff can go here if needed
-        root.setBottom(NavBar.create(SceneType.MENU));
+        root.setBottom(NavBar.create(SceneType.STORE));
         return new Scene(root, 640, 480);
     }
 
     private static Scene buildStatsScene(Stage stage, DatabaseManager db, Context context) {
         BorderPane root = new BorderPane();  // doesn't have to stay borderPane
         // other stuff can go here if needed
-        root.setBottom(NavBar.create(SceneType.MENU));
+        root.setBottom(NavBar.create(SceneType.STATS));
         return new Scene(root, 640, 480);
     }
 
     private static Scene buildCreditsScene(Stage stage, DatabaseManager db, Context context) {
         BorderPane root = new BorderPane();  // doesn't have to stay borderPane
         // other stuff can go here if needed
-        root.setBottom(NavBar.create(SceneType.MENU));
         return new Scene(root, 640, 480);
     }
 
     /*
-    private static Scene buildMainScene(Stage stage, DatabaseManager db) {
-        Button loginBtn = new Button("Log In");
-        loginBtn.setOnAction(e ->
-                SceneManager.getInstance().navigateTo(SceneType.DASHBOARD)
-        );
-
-        return new Scene(new StackPane(loginBtn), 640, 480);
-    }
-
-    private static Scene buildDashboardScene(Stage stage, DatabaseManager db) {
-        Button btn = new Button("Dashboard");
-        Button cookiekBtn = new Button("Navigate to Cookie Scene");
-
-        btn.setOnAction(e ->
-                SceneManager.getInstance().navigateTo(SceneType.ADD_ITEM)
-        );
-
-        cookiekBtn.setOnAction(e ->
-                stage.setScene(CookieScene.create(stage))
-        );
-
-        VBox vBox = new VBox(10);
-        vBox.setAlignment(Pos.CENTER);
-        vBox.getChildren().addAll(btn, cookiekBtn);
-
-        return new Scene(vBox, 640, 480);
-    }
-
-    private static Scene buildAddItemScene(Stage stage, DatabaseManager db) {
-
-        Button saveBtn = new Button("Save");
-
-        saveBtn.setOnAction(e ->
-                SceneManager.getInstance().navigateTo(SceneType.DASHBOARD)
-        );
-
-        return new Scene(new StackPane(saveBtn), 640, 480);
-    }
 
     private static Scene buildDBTestScene(Stage stage, DatabaseManager db) {
         // create
